f49716d40c19b12d37f1a05560685f7e0ab5b009
Saveliy250 <savepo@icloud.com>
2025-02-06 19:58:26 +0300

    working on main screen

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     })
 })
+
+const applyBtn = document.getElementById('applyBtn'); // условно, кнопка "Применить"
+
+applyBtn.addEventListener('click', async () => {
+    // Собираем массив выбранных категорий
+    const selectedBtns = document.querySelectorAll('.category-btn.selected');
+    const categories = Array.from(selectedBtns).map(btn => btn.dataset.value);
+})
+
