e1b21d7d5f48a317c0d7a5a46b13b8dae34229f6
Saveliy250 <savepo@icloud.com>
2025-02-14 12:45:19 +0300

    updated footer: now icons changing according to path

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './index.css'
 import {Route, Routes} from "react-router-dom";
 import MainScreen from "./MainScreen.jsx";
 import Filters from "./filters";
+import Favorites from "./FavoritesPage.jsx";
 
 
 
@@ -13,6 +14,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<MainScreen />} />
                 <Route path="/filters" element={<Filters />}  />
+                <Route path="/favorites" element={<Favorites />} />
             </Routes>
         </>
     )
---
+++ src/FavoritesPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './index.css'
+import {Footer} from "./MainScreen.jsx";
+
+function Favorites() {
+    return (
+        <>
+            <Footer />
+        </>
+    )
+}
+export default Favorites;
