f003a21cd969d2675b9f017b1f40d2a69f216f69
Saveliy250 <savepo@icloud.com>
2025-03-08 16:25:15 +0300

    complete task without link

--- src/EventPage.jsx
+++ src/EventPage.jsx
@@ -1,15 +1,23 @@
 import React, {useEffect, useState} from 'react';
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
+import "./EventPageStyle.css";
+import WhiteLogoIco from "./icons/WhiteLogoIco.jsx";
 
 function EventPage() {
     const {eventId} = useParams();
     const [eventData, setEventData] = useState(null);
+    const [noEvent, setNoEvent] = useState(false);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch(`http://90.156.170.125:8080/event-service/api/v1/events/${eventId}`)
             .then((response) => {
+                if (response.status === 204) {
+                    setNoEvent(true);
+                    setLoading(false);
+                    return;
+                }
                 if (!response.ok) {
                     throw new Error("Error fetching event data.");
                 }
@@ -24,26 +32,46 @@ function EventPage() {
             setLoading(false);
         })
     }, [eventId]);
-
+    if (noEvent) return (
+        <div className={"eventPage-204"}>
+            <p>Такого события нет</p>
+            <Link to={'/'}>НА ГЛАВНУЮ</Link>
+        </div>
+    );
     if (loading) return <div>Загрузка...</div>;
-    if (error) return <div>Ошибка: {error.message}</div>;
+    if (error) return <div>Ошибка: {error.code}</div>;
 
     return (
-        <div className={"event-page"}>
-            <div className="event-page-header"></div>
+        <>
+            <div className="event-page-logo"><WhiteLogoIco/></div>
             <div className="event-page-content">
-                <div className={'event-page-img-container'}><img
+                <div className={'event-page-img-wrapper'}><img
                     src={eventData.imageURL[0]}
                     alt={eventData.name}
                     className="event-page-img"
                 />
                 </div>
-                <div className={'event-page-content-text'}>
-                    <p>{eventData.name}</p>
-                    
+                <div className={'event-page-content-wrapper'}>
+                    <div className={"event-name"}>
+                        <svg width="28" height="29" viewBox="0 0 28 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10.4745 18.7925L23.8558 18.7925" stroke="#ECFE54" strokeWidth="3.35446"/>
+                            <path d="M19.0019 10.0492L26.0002 18.8338L19.0019 27.4502" stroke="#ECFE54" strokeWidth="3.35446" strokeLinejoin="round"/>
+                            <path d="M25.9548 2H11.6037C-1.20125 2 -1.20122 18.7921 11.6037 18.7921H25.9548" stroke="#ECFE54" strokeWidth="3.35446"/>
+                        </svg>
+                        <p>{eventData.name}</p>
+                    </div>
+                    <div className={"event-description"}>О мероприятии: <br/>{eventData.description}</div>
+                    <div className={"event-button-arrow"}>
+                        <svg width="28" height="29" viewBox="0 0 28 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10.4745 18.7925L23.8558 18.7925" stroke="#ECFE54" strokeWidth="3.35446"/>
+                            <path d="M19.0019 10.0492L26.0002 18.8338L19.0019 27.4502" stroke="#ECFE54" strokeWidth="3.35446" strokeLinejoin="round"/>
+                            <path d="M25.9548 2H11.6037C-1.20125 2 -1.20122 18.7921 11.6037 18.7921H25.9548" stroke="#ECFE54" strokeWidth="3.35446"/>
+                        </svg>
+                        <button className={"event-button"}>Перейти на сайт мероприятия</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 
 }
