fd87c159d92ef54165d16b3129d4eef8ac86e55a
Saveliy250 <savepo@icloud.com>
2025-04-04 12:01:23 +0500

    add login page

--- src/LogInPage.jsx
+++ src/LogInPage.jsx
@@ -1,7 +1,33 @@
+import useAuth from "./hooks/useAuth.js";
+import {useState} from "react";
+
 function LogInPage() {
+
+    const {login,loading, error, isAuthenticated} = useAuth();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        login(username, password);
+    };
+
+    if (isAuthenticated) {
+        return <div>You already logged in</div>
+    }
+
     return (
         <>
-            <p>LOGIN</p>
+            <div>Вход в аккаунт</div>
+            <form onSubmit={handleSubmit}>
+                <label>Логин</label>
+                <input value={username} onChange={(e) => setUsername(e.target.value)} />
+                <label>Пароль</label>
+                <input type={"password"} value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button type="submit" disabled={loading}>
+                    {loading ? `Загрузка...` : `Войти`}
+                </button>
+            </form>
         </>
     )
 }
