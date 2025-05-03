30f797c1f565e7b4524916faab99ba9d84319006
Saveliy250 <savepo@icloud.com>
2025-05-03 18:55:09 +0300

    vite confg changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react() ],
   server: {
     host: '0.0.0.0',
     allowedHosts: ['ui.dada-tuda.ru', 'dada-tuda.ru'],
-    https: true,
   },
 })
