53df7c92ae20ffefb689dda1574b136094376dd7
Алекс <lexagri200430@gmail.com>
2025-07-10 19:11:57 +0000

    vite.config.js edited online with Bitbucket

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
     plugins: [react()],
     server: {
         host: '0.0.0.0',
-        allowedHosts: ['ui.dada-tuda.ru', 'dada-tuda.ru', 'tg-miniapp-dada-b1.dada-tuda.ru'],
+        allowedHosts: ['ui.dada-tuda.ru', 'tg-miniapp-dada-b1.dada-tuda.ru'],
     },
 });
