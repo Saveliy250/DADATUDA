82514542ee2122ff67b39da6f86bc9e9a1c48f8e
Дима Колотушкин <skkogame@gmail.com>
2025-07-28 16:17:39 +0300

    DADA-225: Main Page redesign/rework

---
+++ src/shared/icons/MainPage/CurvedArrowIcon.jsx
@@ -0,0 +1,18 @@
+export const CurvedArrowIcon = () => {
+    return (
+        <svg width="26" height="28" viewBox="0 0 26 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9.47363 17.7915L22.8548 17.7915" stroke="#F2FF87" strokeWidth="2" />
+            <path
+                d="M18.0007 9.04932L24.999 17.8339L18.0007 26.4502"
+                stroke="#F2FF87"
+                strokeWidth="2"
+                strokeLinejoin="round"
+            />
+            <path
+                d="M24.9547 1H10.6036C-2.20123 1 -2.2012 17.792 10.6036 17.792H24.9547"
+                stroke="#F2FF87"
+                strokeWidth="2"
+            />
+        </svg>
+    );
+};
