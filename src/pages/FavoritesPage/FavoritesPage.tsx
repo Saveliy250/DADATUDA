ba3b72e63746e0f70bdf6b0e1232070486b748dd
Saveliy250 <savepo@icloud.com>
2025-09-18 15:27:07 +0300

    DADA-264: ya togo vse ebal

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { toggleFavorite, sendFeedback, getShortlist } from '../../tools/api/api';
 import { readCachedFeedback, clearCachedFeedback, writeCachedFeedback } from '../../tools/feedbackCache';
 import { useNavigate } from 'react-router-dom';
